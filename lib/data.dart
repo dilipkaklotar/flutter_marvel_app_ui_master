84e5e1ceff8ac47531d088c68b3920a726edba89
dilipkumar <dilip143neet>
2019-05-28 15:30:08 +0530

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/spiderman.jpg",
+  "assets/superman.jpeg",
+  "assets/thedarkknightrises.jpg",
+  "assets/aquaman.jpg",
+];
+
+List<String> title = [
+  "Spiderman",
+  "Superman",
+  "The Dark Knight Rises",
+  "Aquaman",
+];
